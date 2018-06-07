04615876ddc044351024b51b1676e3621d33da35
方觉 (Fang Jue) <fangjue23303@gmail.com>
2018-06-07 09:38:42 +0800

    Updates for sz_buses_parser.py
    * Support incremental parsing to avoid repeated parsing already parsed data.
    * Add --json option to get a pure JSON output.
    * Add verbosity options to better aid debugging.
    * Optimize numbers in JSON output to save space.

--- sz_buses_parser.py
+++ sz_buses_parser.py
@@ -9,8 +9,9 @@ import datetime, json, os.path, signal
 
 DIR = os.path.dirname(__file__)
 JS_DATA_HEADER = 'var __BUS_LINE_DATA='
-INCREMENTAL_STATE_FILE='_last_known_state.json'
-PROCESSED_DATA_FILE='_last_processed_data.json'
+INCREMENTAL_STATE_FILE='_last_known_state%s.json'
+PROCESSED_DATA_FILE='_last_processed_data%s.json'
+DATE_RANGE_KEY = '___/*DATE_RANGE___'
 
 _Verbose = 0
 _CtrlC = 0
@@ -39,9 +40,9 @@ def _GetPreviousDate(date):
 
 _GetPreviousDate.cache = {}
 
-def _LoadIncrementalState():
-  path_state = os.path.join(DIR, INCREMENTAL_STATE_FILE)
-  path_data = os.path.join(DIR, PROCESSED_DATA_FILE)
+def _LoadIncrementalState(namespace):
+  path_state = os.path.join(DIR, INCREMENTAL_STATE_FILE % namespace)
+  path_data = os.path.join(DIR, PROCESSED_DATA_FILE % namespace)
   if os.path.isfile(path_state) and os.path.isfile(path_data):
     with open(path_state, 'r') as state_file:
       with open(path_data, 'r') as data_file:
@@ -51,24 +52,28 @@ def _LoadIncrementalState():
     sys.stderr.write('Incremental state file does not exist. Doing a full parse...\n')
   return ({}, {})
 
-def _SaveIncrementalState(state, data):
+def _SaveIncrementalState(namespace, state, data):
   try:
-    with open(os.path.join(DIR, INCREMENTAL_STATE_FILE), 'w') as state_file:
-      with open(os.path.join(DIR, PROCESSED_DATA_FILE), 'w') as data_file:
+    with open(os.path.join(DIR, INCREMENTAL_STATE_FILE % namespace), 'w') as state_file:
+      with open(os.path.join(DIR, PROCESSED_DATA_FILE % namespace), 'w') as data_file:
         json.dump(state, state_file, separators=(',', ':'), sort_keys=True)
         json.dump(data, data_file, separators=(',', ':'), sort_keys=True)
   except:
     if _Verbose:
       sys.stderr.write('Error saving incremental state file.\n')
 
+def _FormatWeightValue(times, total):
+  if times == 0:
+    return '0'
+  rounded_value = round(float(times) / total, 3)
+  if int(rounded_value) == rounded_value:
+    return str(int(rounded_value))
+  return str(rounded_value)
+
 if __name__ == '__main__':
   if len(sys.argv) <= 1:
     print 'Usage: sz_buses_parser.py [-v|--verbose] [-i|--incremental] <line...>'
     sys.exit()
-
-  incremental = False
-  incremental_state = {}
-  processed_data = {}
 
   buses_map = {}
   with open('buses', 'r') as f:
@@ -77,8 +82,11 @@ if __name__ == '__main__':
       if len(values) >= 2:
         buses_map[values[1]] = values[0]
 
-#  print '''<!doctype html>
   lines = []
+  incremental = False
+  date_range = None
+  pure_json = False
+  namespace = ''
   for line in sys.argv[1:]:
     if line.startswith('-'):
       if line == '-v' or line == '--verbose' or line == '-V1':
@@ -92,7 +100,21 @@ if __name__ == '__main__':
         continue
       if line == '-i' or line == '--incremental':
         incremental = True
-        incremental_state, processed_data = _LoadIncrementalState()
+        continue
+      if line == '--json':
+        pure_json = True
+        continue
+      arg_range = '--range='
+      if line.startswith(arg_range):
+        date_range = line[len(arg_range):].split(',')
+        if len(date_range) == 2:
+          if len(date_range[1]) == 0:
+            date_range[1] = '9999-99-99'
+          date_range = (date_range[0], date_range[1])
+        continue
+      arg_namespace = '--namespace='
+      if line.startswith(arg_namespace):
+        namespace = '_%s' % line[len(arg_namespace):]
         continue
 
     linefile = line
@@ -103,9 +125,23 @@ if __name__ == '__main__':
 
     lines.append((line, linefile))
 
-#  print '<script type="application/json" id="lineData">{'
-  sys.stdout.write(JS_DATA_HEADER)
-  sys.stdout.write('JSON.parse(\'{')
+  incremental_state = {}
+  processed_data = {}
+  if incremental:
+    incremental_state, processed_data = _LoadIncrementalState(namespace)
+  if date_range is not None:
+    if incremental_state.get(DATE_RANGE_KEY) is None or incremental_state[DATE_RANGE_KEY] != list(date_range):
+      if _Verbose:
+        sys.stderr.write('Incremental state is overwritten because of date range mismatch.\n')
+      incremental_state = {DATE_RANGE_KEY: list(date_range)}
+      processed_data = {}
+  if pure_json:
+    sys.stdout.write('{')
+    line_break = '\n'
+  else:
+    sys.stdout.write(JS_DATA_HEADER)
+    sys.stdout.write('JSON.parse(\'{')
+    line_break = '\\\n'
 
   first = True
 
@@ -133,7 +169,7 @@ if __name__ == '__main__':
             running_buses_set = set(processed_data[line]['running_buses_set'])
             f.seek(state['fp'])
 
-            if _Verbose >= 3:
+            if _Verbose >= 2:
               sys.stderr.write('Incrementally processing %s beginning at offset %s, after date %s\n' % (linefile, state['fp'], state['date']))
           elif _Verbose:
             sys.stderr.write('Incomplete incremental state for line %s.\n' % line)
@@ -158,7 +194,10 @@ if __name__ == '__main__':
             date = current_date
           if current_date is None:
             current_date = date
-          elif current_date != date and not fp_frozen:
+          if len(date) > 0 and date_range is not None:
+            if _CompareDate(date, date_range[0]) < 0 or _CompareDate(date, date_range[1]) > 0:
+              continue
+          if incremental and  current_date != date and not fp_frozen:
             if _CompareDate(date, current_date) > 0:
               if incremental_state.get(linefile) is None:
                 incremental_state[linefile] = {}
@@ -167,11 +206,11 @@ if __name__ == '__main__':
               incremental_state[linefile]['date'] = current_date
               previous_date_ending_fp = last_fp
               incremental_state[linefile]['fp'] = last_fp
-              current_date = date
               if processed_data.get(line) is None:
                 processed_data[line] = {}
               processed_data[line]['running_buses'] = running_buses
               processed_data[line]['running_buses_set'] = list(running_buses_set)
+              current_date = date
             else:
               fp_frozen = True
               if _Verbose:
@@ -180,8 +219,14 @@ if __name__ == '__main__':
           # For night lines, |date| means the date of the night when the first bus appears.
           if night_line and ':' in time and int(time.split(':')[0]) < 12:
             date = _GetPreviousDate(date)
-          bus_id = values[2]
+          bus_id = values[2].replace('\\u82cf', '苏')
           prefix = '苏E-'
+          if bus_id.startswith(prefix):
+            bus_id = bus_id[len(prefix):]
+          prefix = '苏E'
+          if bus_id.startswith(prefix):
+            bus_id = bus_id[len(prefix):]
+          prefix = '苏'
           if bus_id.startswith(prefix):
             bus_id = bus_id[len(prefix):]
           running_buses_set.add(bus_id)
@@ -192,9 +237,8 @@ if __name__ == '__main__':
               running_buses[date][bus_id] =  1
             else:
               running_buses[date][bus_id] += 1
-        elif _Verbose >= 2:
-          pass
-          #sys.stderr.write('Skipped invalid entry for %s: %s' % (line, l)) # |l| has a trailing '\n'.
+        elif _Verbose >= 3:
+          sys.stderr.write('Skipped invalid entry for %s: %s' % (line, l)) # |l| has a trailing '\n'.
 
     if len(running_buses) > 0:
       sorted_running_buses = sorted(running_buses_set, key=lambda x:buses_map.get(x, 'Z%s' % x))
@@ -203,15 +247,16 @@ if __name__ == '__main__':
         first = False
       else:
         sys.stdout.write(',')
-      sys.stdout.write('\\\n"%s":{"details":[' % line)
+      sys.stdout.write('%s"%s":{"details":[' % (line_break, line))
 
-      sys.stdout.write(',\\\n'.join([
+      sys.stdout.write((',%s' % line_break).join([
         '["%s",[%s]]' % (date,
-          ','.join([str(round(float(buses.get(bus_id, 0)) / max(buses.values()), 3))
+          ','.join([_FormatWeightValue(buses.get(bus_id, 0), max(buses.values()))
             for bus_id in sorted_running_buses]))
         for date, buses in sorted(running_buses.iteritems(), key=lambda x:x[0])])
       )
-      sys.stdout.write('\\\n],"buses":[%s]}' % ','.join(['{"busId":"%s","licenseId":"%s"}' % (buses_map.get(x, ''), x.encode('utf-8')) for x in sorted_running_buses]))
+      sys.stdout.write('%s],"buses":[%s]}' % (line_break, 
+          ','.join(['{"busId":"%s","licenseId":"%s"}' % (buses_map.get(x, ''), x.encode('utf-8')) for x in sorted_running_buses])))
 
     elif _Verbose:
       sys.stderr.write('Skipped line with empty data: %s\n' % line)
@@ -220,291 +265,10 @@ if __name__ == '__main__':
       sys.stderr.write('\nCtrl-C pressed. Aborting...\n')
       break
 
-  print '}\')'
+  if pure_json:
+    sys.stdout.write('}')
+  else:
+   sys.stdout.write('}\')')
 
   if incremental:
-    _SaveIncrementalState(incremental_state, processed_data)
-
-
-
-
-#  print '''}</script>
-  '''    <script>
-      var COLOR = [106, 90, 205];
-      var PALETTE = [
-        [106, 90, 205],
-        [34, 139, 34],
-        [178, 34, 34],
-        [218, 165, 32],
-        [128, 0, 128],
-      ];
-
-      var lineData = {};
-      var activeLines = [];
-
-      function fillTr(data, th, attrs) {
-        var tr = document.createElement('tr');
-        [].forEach.call(data, function(item, index) {
-          var td = document.createElement(th ? 'th' : 'td');
-          if (attrs && attrs[index])
-          for (var x in attrs[index])
-            td.setAttribute(x, attrs[index][x]);
-          td.appendChild(document.createTextNode(item));
-          tr.appendChild(td);
-        });
-        return tr;
-      }
-
-      function removeChildren(parent) {
-        while(parent.hasChildNodes())
-          parent.removeChild(parent.childNodes[0]);
-      }
-
-      function showLine(line) {
-        activeLines = [line];
-        var content = document.getElementById('content');
-        removeChildren(content);
-        if (!lineData[line]) {
-          content.appendChild(document.createTextNode('Line ' + line + ' does not exist!'));
-          return;
-        }
-
-        var table = document.createElement('table');
-        var tbody = document.createElement('tbody');
-        tbody.appendChild(fillTr(["Bus ID"].concat(lineData[line].buses.map(bus => bus.busId)), true));
-        tbody.appendChild(fillTr(["License ID"].concat(lineData[line].buses.map(bus => bus.licenseId)), true));
-        lineData[line].details.forEach(function(day) {
-          tbody.appendChild(fillTr([day[0]].concat(new Array(day[1].length).fill('')), false, [''].concat(day[1].map(function(weight){
-            return {style: 'background-color:rgb(' + COLOR.map(function(value) {
-              return parseInt((255 - value) * (1 - weight) + value);
-            }).join(',') + ')'};
-          }))));
-        });
-        table.appendChild(tbody);
-        content.appendChild(table);
-      }
-
-      function findBusById(query, busId) {
-        var resultList = document.getElementById('resultList');
-        removeChildren(resultList);
-        for (var line in lineData) {
-          if (lineData[line].buses.some(function(bus) {
-            if (busId)
-              return bus.busId[0] == query[0] && bus.busId.substr(2) == query.substr(2);
-            else
-              return bus.licenseId == query;
-          })) {
-            var option = document.createElement('option');
-            option.value = line;
-            option.appendChild(document.createTextNode(line));
-            resultList.appendChild(option);
-          }
-        }
-      }
-
-      function showLines(lines) {
-        var content = document.getElementById('content');
-        var legend = document.getElementById('legend');
-        removeChildren(content);
-        removeChildren(legend);
-
-        if (lines.length > PALETTE.length) {
-          content.appendChild(document.createTextNode('Too many lines selected!'));
-          return;
-        }
-
-        if (lines.some(line => !lineData[line])) {
-          content.appendChild(document.createTextNode('Not all lines exist!'));
-          return;
-        }
-
-        for (var i = 0; i < lines.length; ++i) {
-          var item = document.createElement('span');
-          var span = document.createElement('span');
-          span.style.backgroundColor = 'rgb(' + PALETTE[i].join(',') + ')';
-          span.style.height = '1em';
-          span.style.width = '2em';
-          span.style.display = 'inline-block';
-          item.style.marginLeft = '3em';
-          item.appendChild(span);
-          item.appendChild(document.createTextNode(' ' + lines[i]));
-          legend.appendChild(item);
-        }
-
-        var allBuses = [];
-        var licenseIdSet = new Set();
-        var lineDetailsMap = {};
-        for (var i = 0; i < lines.length; ++i) {
-          currentLineData = lineData[lines[i]];
-
-          currentLineData.buses.forEach(function(bus) {
-            if (!licenseIdSet.has(bus.licenseId)) {
-              allBuses.push(bus);
-              licenseIdSet.add(bus.licenseId);
-            }
-          });
-          currentLineData.details.forEach(function(day) {
-            if (!lineDetailsMap[day[0]]) {
-              lineDetailsMap[day[0]] = {};
-            }
-            for (var j = 0; j < currentLineData.buses.length; ++j) {
-              var licenseId = currentLineData.buses[j].licenseId;
-              if (!lineDetailsMap[day[0]][licenseId])
-                lineDetailsMap[day[0]][licenseId] = new Array(lines.length).fill(0);
-              lineDetailsMap[day[0]][licenseId][i] = day[1][j];
-            }
-          });
-        }
-
-        allBuses.sort(function(a, b) {
-          if (a.busId && b.busId) {
-            if (a.busId < b.busId)
-              return -1;
-            else if (a.busId > b.busId)
-              return 1;
-            else
-              return 0;
-          } else if (a.busId && !b.busId) // a < b
-            return -1;
-          else if (!a.busId && b.busId) // a > b
-            return 1;
-          else {
-            if (a.licenseId < b.licenseId)
-              return -1;
-            else if (a.licenseId > b.licenseId)
-              return 1;
-            return 0;
-          }
-        });
-
-        var table = document.createElement('table');
-        var tbody = document.createElement('tbody');
-        tbody.appendChild(fillTr(["Bus ID"].concat(allBuses.map(bus => bus.busId)), true));
-        tbody.appendChild(fillTr(["License ID"].concat(allBuses.map(bus => bus.licenseId)), true));
-        Object.keys(lineDetailsMap).sort().forEach(function(date) {
-          var tr = document.createElement('tr');
-          var th = document.createElement('th');
-          th.appendChild(document.createTextNode(date));
-          tr.appendChild(th);
-          allBuses.forEach(function(bus) {
-            var td = document.createElement('td');
-            tr.appendChild(td);
-            var activeCount = 0;
-            if (lineDetailsMap[date][bus.licenseId]) {
-              activeCount = lineDetailsMap[date][bus.licenseId].filter(x => x > 0).length;
-            }
-            if (activeCount == 0)
-              return;
-
-            lineDetailsMap[date][bus.licenseId].forEach(function(weight, index) {
-              if (weight > 0) {
-                var children = document.createElement('span');
-                children.style.height = '100%';
-                children.style.width = 'calc(100%/' + activeCount + ')';
-                children.style.display = 'inline-block';
-                children.style.backgroundColor = 'rgb(' + PALETTE[index].map(function(value) {
-                  return parseInt((255 - value) * (1 - weight) + value);
-                }).join(',') + ')';
-                children.appendChild(document.createTextNode('\u00a0'));
-                td.appendChild(children);
-              }
-            });
-          });
-          tbody.appendChild(tr);
-        });
-        table.appendChild(tbody);
-        content.appendChild(table);
-      }
-
-      function onChooseLineEx() {
-        if (document.getElementById('compare').checked) {
-          if (activeLines.includes(this.value))
-            return;
-          activeLines.push(this.value);
-          history.pushState(activeLines, '', '#' + activeLines.join('+'));
-          showLines(activeLines);
-        } else {
-          var line = this.value;
-          showLine(line);
-          history.pushState(line, '', '#' + line);
-        }
-      }
-
-      document.addEventListener('DOMContentLoaded', function() {
-        var lineChooser = document.getElementById('lineChooser');
-        lineChooser.addEventListener('change', onChooseLineEx);
-        document.getElementById('resultList').addEventListener('change', onChooseLineEx);
-        window.onpopstate = function(e) {
-          if (e.state instanceof Array) {
-            activeLines = e.state;
-            showLines(activeLines);
-          } else {
-            showLine(e.state);
-          }
-        };
-        document.getElementById('busid').addEventListener('input', function() {
-          var query = this.value;
-          if (query.length == 5)
-            findBusById(query, false);
-          else if (query.length == 6)
-            findBusById(query, true);
-        });
-        lineData = JSON.parse(document.getElementById('lineData').innerHTML);
-        if (location.hash.replace('#', '')) {
-          var hashValue = location.hash.replace('#', '');
-          if (hashValue.includes('+')) {
-            activeLines = hashValue.split('+');
-            showLines(hashValue.split('+'));
-          } else
-            showLine(lineChooser.value = hashValue);
-        } else
-          showLine(lineChooser.children[0].value);
-      });
-    </script>
-  </head>
-  <body>'''
-#  print '<p>Choose line: </h3><select id="lineChooser">'
-#  for line, linefile in lines:
-#    print '<option value="%s">%s</option>' % (line, line)
-#  print '''</select>
-  '''      <label><input type="checkbox" id="compare">+ Compare</label>
-      <span style="margin-left: 3em">
-        Find bus:
-        <input type="text" id="busid">
-        <select size="5" id="resultList"></select>
-      </span>
-    </p>
-    <div id="legend"></div>
-    <div id="content"></div>
-  </body>
-</html>'''
-  '''<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Buses</title>
-    <style>
-      col.bus_id {
-        background-color: LightGoldenRodYellow;
-      }
-      col.direction_0 {
-        background-color: PaleTurquoise;
-      }
-      col.direction_1 {
-        background-color: Plum;
-      }
-
-      #contents {
-        column-count: 4;
-        column-rule: 1px solid grey;
-        column-gap: 5em;
-      }
-
-      td.active {
-        background-color: SkyBlue;
-      }
-
-      th, td:nth-child(1) {
-        white-space: nowrap;
-      }
-    </style>'''
-
+    _SaveIncrementalState(namespace, incremental_state, processed_data)
